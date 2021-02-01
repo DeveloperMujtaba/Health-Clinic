195078fca0d512b5f4c10c7b5c2a667cf504ced8
Najam Khan <ghulammujtabakhan@outlook.com>
2021-02-01 22:05:34 +0500

    Add project files.

---
+++ HealthClinic/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HealthClinic.MvcApplication" Language="C#" %>
---
+++ HealthClinic/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HealthClinic.Startup))]
+namespace HealthClinic
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
