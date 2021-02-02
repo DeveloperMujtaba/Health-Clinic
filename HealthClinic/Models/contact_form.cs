65d503799690d6235c421e8a217126fbabf90c2d
Najam Khan <ghulammujtabakhan@outlook.com>
2021-02-03 01:12:37 +0500

    updated files

---
+++ HealthClinic/Models/contact_form.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HealthClinic.Models
+{
+    public class contact_form
+    {
+        public string input_1 { get; set; }
+        public string input_2 { get; set; }
+        public string input_3 { get; set; }
+        public string input_4 { get; set; }
+        public string input_5 { get; set; }
+        public string input_6 { get; set; }
+        public string input_7 { get; set; }
+        public string input_8 { get; set; }
+        public DateTime input_9 { get; set; }
+        public DateTime input_10 { get; set; }
+        public string input_11 { get; set; }
+        public string input_12 { get; set; }
+        public string input_13 { get; set; }
+        public string input_14 { get; set; }
+        public string input_15 { get; set; }
+    }
+}
---
+++ HealthClinic/Models/driverfleet.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HealthClinic.Models
+{
+    public class driverfleet
+    {
+        public string input_1 { get; set; }
+        public string input_2 { get; set; }
+        public string input_3 { get; set; }
+        public string input_4 { get; set; }
+        public string input_5 { get; set; }
+        public string input_6_1 { get; set; }
+        public string input_6_2_1 { get; set; }
+        public string input_6_2_2 { get; set; }
+        public string input_6_3_1 { get; set; }
+        public string input_6_3_2 { get; set; }
+        public string input_6_3_3 { get; set; }
+        public string input_6_3_4 { get; set; }
+        public string input_6_3_5 { get; set; }
+        public string input_6_3_6 { get; set; }
+        public string input_6_4 { get; set; }
+        public string input_6_5 { get; set; }
+        public string input_6_6 { get; set; }
+        public string input_7 { get; set; }
+        public string input_8 { get; set; }
+    }
+}
